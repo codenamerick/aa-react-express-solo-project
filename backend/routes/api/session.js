2802e8a0874ff59e41d87f930630c26409e4c632
Rick Arocho <rick.arocho@gmail.com>
2021-11-05 17:37:26 -0500

    added validation handling to login route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,8 +4,22 @@ const asyncHandler = require('express-async-handler');
 const {setTokenCookie, restoreUser} = require('../../utils/auth');
 const {User} = require('../../db/models');
 
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+
 const router = express.Router();
 
+
+const validateLogin = [
+    check('credential')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
 
 // restore user session
 router.get('/', restoreUser, (req, res) => {
@@ -20,7 +34,7 @@ router.get('/', restoreUser, (req, res) => {
 });
 
 // this is used for login
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
     const {credential, password} = req.body;
 
     const user = await User.login({credential, password});
