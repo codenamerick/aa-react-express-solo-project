1e47d24ed13ca5a3122d4a3e5ba8e82c12368f3f
Rick Arocho <rick.arocho@gmail.com>
2021-11-05 15:11:40 -0500

    created express routes for users and session

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const {User} = require('../../db/models');
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+// const asyncHandler = require('express-async-handler');
+// const {User} = require('../../db/models');
+// const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({requestBody: req.body});
