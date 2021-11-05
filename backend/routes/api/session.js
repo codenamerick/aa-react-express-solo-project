8ac5b44531e6755dfe2119a7f2dfcb46f4be333e
Rick Arocho <rick.arocho@gmail.com>
2021-11-05 16:14:23 -0500

    coded logout route and tested func

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,6 +29,12 @@ router.post('/', asyncHandler(async (req, res, next) => {
     });
 }));
 
+// logout route below
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({message: 'success'});
+});
+
 
 
 module.exports = router;
