2571140d7c54f609348b7f1f93951c675c4d6974
Rick Arocho <rick.arocho@gmail.com>
2021-11-07 01:17:13 -0500

    create LoginForm component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import React from "react";
+import {Route, Switch} from 'react-router-dom';
+import LoginForm from "./components/LoginForm";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path='/login'>
+        <LoginForm />
+      </Route>
+    </Switch>
   );
 }
 
