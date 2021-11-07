1918ece4f441b4c58143ce45d690ff9ee2497c75
Rick Arocho <rick.arocho@gmail.com>
2021-11-06 21:26:28 -0500

    added route to connect frontend w/backend in backend routes index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,35 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+// static routes
+// serve react build files in production
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static(path.resolve('../frontend/build')));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
 router.get('/hello/world', function(req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
