8c7739635393ef22c5c9d50082541ab04c1ec325
Rick Arocho <rick.arocho@gmail.com>
2021-11-06 21:10:28 -0500

    create csrf.js and custom csrfFetch func

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie';
+
+export async function csrfFetch(url, options = {}) {
+    // setting method to 'GET' if there is no method
+    options.method = options.method || 'GET';
+    // setting headers to empty obj in the event there is no headers
+    options.headers = optoins.headers || {};
+
+    if (options.method.toUpperCase() !== 'GET') {
+        options.headers['Content-Type'] =
+            options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    }
+
+    const res = await window.fetch(url, options);
+
+    if (res.status >= 400) throw res;
+
+    return res;
+};
