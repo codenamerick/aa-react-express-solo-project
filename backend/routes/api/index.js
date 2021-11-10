fa38eea6f8ff458788c07ce2a5f68aa81f2f7202
Rick Arocho <rick.arocho@gmail.com>
2021-11-10 12:56:21 -0600

    created get route for songs

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 // const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const sessionRouter = require('./session');
 const usersRouter = require('./users');
+const songsRouter = require('./songs')
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/songs', songsRouter);
 
 // router.post('/test', function(req, res) {
 //     res.json({requestBody: req.body});
